5ada4776235df9dd257e4389e826f97b44949e62
Uliana Sadykova <ulanasadikova65@gmail.com>
2021-01-26 15:48:45 +0200

    use class to add menucards on page

--- js/main.js
+++ js/main.js
@@ -139,4 +139,67 @@ tabsParent.addEventListener('click', function(event) {
    }
 
    window.addEventListener('scroll', showModalByScroll);
+   
+   //Use classes for cards
+   class Cards {
+      constructor(src, alt, menuSubtitle, description, price, parentSelector){
+         this.src = src;
+         this.alt = alt;
+         this.menuSubtitle = `Меню "${menuSubtitle}"`;
+         this.description = description;
+         this.price = price;
+         this.parent = document.querySelector(parentSelector);
+         this.transfer = 27;
+         this.changeToUAH();
+      }
+
+      changeToUAH() {
+         this.price = this.price * this.transfer;
+      }
+
+      render() {
+         const element = document.createElement('div');
+         element.innerHTML = `<div class="menu__item">
+         <img src= ${this.src} alt= ${this.alt}>
+         <h3 class="menu__item-subtitle">${this.menuSubtitle}</h3>
+         <div class="menu__item-descr">${this.description}</div>
+         <div class="menu__item-divider"></div>
+         <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+         </div>
+         </div>`;
+
+         this.parent.append(element);
+      }
+   }
+
+   new Cards(
+      "img/tabs/vegy.jpg", 
+      "vegy", 
+      "Фитнес", 
+      `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. 
+      Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`, 
+      8, 
+      ".menu .container"
+      ).render();
+
+   new Cards(
+      "img/tabs/elite.jpg",
+      "elite",
+      'Премиум',
+      `В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
+      20,
+      '.menu .container'
+   ).render();
+
+   new Cards(
+      "img/tabs/post.jpg",
+      "post",
+      "Постное",
+      `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.`,
+      17,
+      '.menu .container'
+   ).render();
+
 });
